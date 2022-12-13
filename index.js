878bc3e590e06221cd50b9577685be497f50fbb9
agbolahan babatunde <akanniagbolahan@gmail.com>
2022-12-13 10:29:55 -1100

    added error handler middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const TasksRoute = require('./routes/tasksRoute.js')
 const NotFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/Error')
 
 const app = express();
 
@@ -10,16 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use('/api/v1/tasks', TasksRoute);
 app.use(NotFound)
+app.use(errorHandler)
 app.get('/', (req, res) => {
     res.status(200).send({
         message: 'Welcome to  Task Manager App'
     })
 });
 
-app.use((err, req, res, next) => {
-    res.status(500).send({
-        error: err.message
-    })
-});
 
 module.exports = app
